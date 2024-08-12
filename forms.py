8dad59b67310fce7d9e559bb39c0bbecdcc28bb8
Oluwadamilare <oguntadeoluwadamilare50@gmail.com>
2024-08-12 01:52:04 +0100

    More forms and html files were created to handle the forget password, , older post button got removed, and a forget password button got added

--- forms.py
+++ forms.py
@@ -41,7 +41,7 @@ class CommentForm(FlaskForm):
 
 # Forget Password Form
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
     submit = SubmitField("Request Password Reset")
 
 
